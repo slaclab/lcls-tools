c60c6b87b3679ccf41129fe853f834aec4698778
dabisi <dabisi@slac.stanford.edu>
2021-08-17 14:06:22 -0700

    Added comments, rack class, and changed class hierarchy

--- lcls_tools/devices/scLinac.py
+++ lcls_tools/devices/scLinac.py
@@ -2,33 +2,90 @@
 # Utility classes for superconduting linac
 ###########################################################
 
+
 class Linac:
     def __init__(self, name, cryomoduleStringList):
     	# type: (str, List[str]) -> None
+        '''
+        Parameters
+        ----------
+        name: str name of Linac i.e. "L0B", "L1B", "L2B", "L3B"
+        cryomoduleStringList: list of string names of cryomodules in the linac
+        '''
+        
         self.name = name
         self.cryomodules = {}
         for cryomoduleString in cryomoduleStringList:
-            self.cryomodules[cryomoduleString] = Cryomodule(cryomoduleString, self)
+            self.cryomodules[cryomoduleString] = self.Cryomodule(cryomoduleString, self)
 
-class Cryomodule:
-    def __init__(self, cryoName, linacObject):
-    	# type: (str, Linac) -> None
-        self.name = cryoName
-        self.linac = linacObject
-        self.cavities = {}
-		# Every cryomodule has 8 cavities, so this is hard coded
-        for cavityNum in range(1, 9):
-            self.cavities[cavityNum] = Cavity(cavityNum, self)
+    class Cryomodule:
+        def __init__(self, cryoName, linacObject):
+        	# type: (str, Linac) -> None
+        	'''
+            Parameters
+            ----------
+            cryoName: str name of Cryomodule i.e. "02", "03", "H1", "H2"
+            linacObject: the linac object this cryomodule belongs to i.e. CM02 is in linac L1B
+            '''
 
-class Cavity:
-    def __init__(self, cavityNum, cryoObject):
-    	# type: (int, Cryomodule) -> None
-        self.number = cavityNum
-        self.cryomodule = cryoObject
-        self.linac = self.cryomodule.linac
-        self.pvPrefix = "ACCL:{LINAC}:{CRYOMODULE}{CAVITY}0:".format(LINAC=self.linac.name,
-                                                                     CRYOMODULE=self.cryomodule.name,
-                                                                     CAVITY=self.number)
+            self.name = cryoName
+            self.linac = linacObject
+            
+            self.pvPrefix = "ACCL:{LINAC}:{CRYOMODULE}00:".format(LINAC=self.linac.name,
+                                                                  CRYOMODULE=self.name)
+            
+            self.racks = {}
+            self.racks["A"] = self.Rack("A", self)
+            self.racks["B"] = self.Rack("B", self)
+
+            self.cavities = dict(self.racks["A"].cavities, **self.racks["B"].cavities)
+
+        class Rack:
+            def __init__(self, rackName, cryoObject):
+                # type: (str, Cryomodule) -> None
+            	'''
+                Parameters
+                ----------
+                rackName: str name of rack (always either "A" or "B")
+                cryoObject: the cryomodule object this rack belongs to
+                '''
+
+                self.cryomodule = cryoObject
+                self.rackName = rackName
+                self.cavities = {}
+                self.pvPrefix = self.cryomodule.pvPrefix
+
+                if rackName == "A":
+                    # rack A always has cavities 1 - 4
+                    for cavityNum in range(1,5):
+                        self.cavities[cavityNum] = self.Cavity(cavityNum, self)
+
+                elif rackName == "B":
+                    # rack B always has cavities 5 - 8
+                    for cavityNum in range(5,9):
+                        self.cavities[cavityNum] = self.Cavity(cavityNum, self)
+
+                else:
+                    raise("Bad rack name")
+                        
+        
+            class Cavity:
+                def __init__(self, cavityNum, rackObject):
+                	# type: (int, Rack) -> None
+                	'''
+                    Parameters
+                    ----------
+                    cavityNum: int cavity number i.e. 1 - 8
+                    rackObject: the rack object the cavities belong to
+                    '''
+                
+                    self.number = cavityNum
+                    self.rack = rackObject
+                    self.cryomodule = self.rack.cryomodule
+                    self.linac = self.cryomodule.linac
+                    self.pvPrefix = "ACCL:{LINAC}:{CRYOMODULE}{CAVITY}0:".format(LINAC=self.linac.name,
+                                                                                 CRYOMODULE=self.cryomodule.name,
+                                                                                 CAVITY=self.number)
 
 # Global list of superconducting linac objects
 LINACS = [Linac("L0B", ["01"]),
