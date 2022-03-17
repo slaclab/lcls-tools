8363efdfc7bb0f3eec956b7ab6cde905f7e08aa2
nneveu <nneveu@stanford.edu>
2022-03-17 14:11:25 -0700

    add url, try using setuptools python version req

--- setup.py
+++ setup.py
@@ -1,22 +1,11 @@
 from setuptools import setup, find_packages
 import sys
 
-if sys.version_info < (3, 6):
-    error = """
-lcls-tools does not support Python 2.x, 3.0, 3.1, 3.2, 3.3, 3.4, or 3.5.
-Python 3.6 and above is required. Check your Python version like so:
-python --version
-This may be due to an out-of-date pip. Make sure you have pip >= 9.0.1.
-Upgrade pip like so:
-pip install --upgrade pip
-"""
-    sys.exit(error)
-
-
 setup(
     name='lcls-tools',
     version='0.1.dev0',
     packages=find_packages(),
+    url='https://github.com/slaclab/lcls-tools',
     license='Apache License',
     python_requires='>=3.6',
     long_description=open('README.md').read(), 
