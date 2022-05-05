32c825e90d7df5dff0cce7afa2017476d6a85335
Sebastian Aderhold <aderhold@slac.stanford.edu>
2022-05-05 16:34:30 -0700

    moved popup generators to display utils

--- lcls_tools/common/pydm_tools/displayUtils.py
+++ lcls_tools/common/pydm_tools/displayUtils.py
@@ -1,4 +1,6 @@
-from PyQt5.QtWidgets import QWidget
+from functools import partial
+
+from PyQt5.QtWidgets import QMessageBox, QWidget
 from qtpy.QtCore import Slot
 
 
@@ -11,3 +13,23 @@ def showDisplay(display: QWidget):
 
     # gives the display focus
     display.activateWindow()
+
+
+def make_error_popup(title, expert_edmbutton, exception, action_func):
+    popup = QMessageBox()
+    popup.setIcon(QMessageBox.Critical)
+    popup.setWindowTitle(title)
+    popup.setText(
+        '{error}\nPlease check expert screen and select from the options below'.format(error=exception))
+    popup.addButton('Abort', QMessageBox.RejectRole)
+    popup.addButton('Acknowledge manual completion and continue', QMessageBox.AcceptRole)
+    popup.addButton(expert_edmbutton, QMessageBox.ActionRole)
+    popup.buttonClicked.connect(partial(action_func, popup))
+    popup.exec()
+
+
+def make_info_popup(text):
+    popup = QMessageBox()
+    popup.setIcon(QMessageBox.Information)
+    popup.setText(text)
+    popup.exec()
