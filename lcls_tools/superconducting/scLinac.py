f5c801bf916cd70ddbcc7f3fa96bb7a2b9d42138
Lisa Zacarias <zacarias@slac.stanford.edu>
2022-05-14 11:09:00 -0700

    patching motor write bug

--- lcls_tools/superconducting/scLinac.py
+++ lcls_tools/superconducting/scLinac.py
@@ -16,6 +16,10 @@ class PV(epicsPV):
     def __init__(self, pvname):
         super().__init__(pvname, connection_timeout=0.01)
 
+    def put(self, value, wait=False, timeout=30.0, use_complete=False,
+            callback=None, callback_data=None):
+        super(PV, self).put(value, wait=True)
+
 
 class SSA:
     def __init__(self, cavity):
@@ -132,7 +136,7 @@ class StepperTuner:
                               else utils.MAX_STEPPER_SPEED)
 
         if abs(numSteps) <= maxSteps:
-            self.step_des_pv.put(numSteps)
+            self.step_des_pv.put(abs(numSteps))
             self.issueMoveCommand(numSteps)
             self.restoreDefaults()
         else:
