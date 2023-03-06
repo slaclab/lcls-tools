8da930ffd9f56adb01f37a29b76e586089d77006
Lisa Zacarias <zacarias@slac.stanford.edu>
2023-03-06 11:59:50 -0800

    casting chirp range as an int

--- lcls_tools/superconducting/scLinac.py
+++ lcls_tools/superconducting/scLinac.py
@@ -668,7 +668,7 @@ class Cavity:
         sleep(1)
         if self.detune_best_PV.severity == EPICS_INVALID_VAL:
             if chirp_range < 500000:
-                self.find_chirp_range(chirp_range * 1.25)
+                self.find_chirp_range(int(chirp_range * 1.25))
             else:
                 raise utils.DetuneError(f"{self}: No valid detune found within"
                                         f"+/-500000Hz chirp range")
