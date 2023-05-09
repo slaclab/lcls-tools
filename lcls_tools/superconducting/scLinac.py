ff7245940e4a1a69c9862bf18a35103bea250e38
Lisa Zacarias <zacarias@slac.stanford.edu>
2023-05-09 15:28:52 -0700

    making stepper limit dynamic based on expected steps to resonance

--- lcls_tools/superconducting/scLinac.py
+++ lcls_tools/superconducting/scLinac.py
@@ -498,7 +498,7 @@ class Cavity:
             print(f"Moving stepper for {self} {est_steps} steps")
             
             self.steppertuner.move(est_steps,
-                                   maxSteps=10000000,
+                                   maxSteps=est_steps * 1.1,
                                    speed=utils.MAX_STEPPER_SPEED)
             steps_moved += abs(est_steps)
             
