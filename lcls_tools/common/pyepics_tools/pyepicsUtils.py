7abe63f86639912206176a579bb9ffa332568358
Lisa Zacarias <zacarias@slac.stanford.edu>
2023-05-26 12:43:16 -0700

    adding ca fallbacks to epics pvs

--- lcls_tools/common/pyepics_tools/pyepicsUtils.py
+++ lcls_tools/common/pyepics_tools/pyepicsUtils.py
@@ -1,8 +1,6 @@
-from datetime import datetime
-from math import isclose
 from time import sleep
 
-from epics import PV as epicsPV
+from epics import PV as epics_pv, caget as epics_caget, caput as epics_caput
 
 # These are the values that decide whether a PV is alarming (and if so, how)
 EPICS_NO_ALARM_VAL = 0
@@ -16,54 +14,52 @@ class PVInvalidError(Exception):
         super(PVInvalidError, self).__init__(message)
 
 
-class PV(epicsPV):
+class PV(epics_pv):
     def __init__(self, pvname):
         super().__init__(pvname, connection_timeout=0.01)
     
+    def caget(self):
+        while True:
+            value = epics_caget(self.pvname)
+            if value is not None:
+                break
+            print(f"{self.pvname} did not return a valid value, retrying")
+            sleep(0.5)
+        return value
+    
+    def caput(self, value):
+        while True:
+            status = epics_caput(self.pvname, value)
+            if status == 1:
+                break
+            print(f"{self} caput did not execute successfully, retrying")
+            sleep(0.5)
+        return status
+    
     def get(self, count=None, as_string=False, as_numpy=True,
             timeout=None, with_ctrlvars=False, use_monitor=True,
-            retry_until_valid=True):
+            retry_until_valid=True, use_caget=False):
         
-        self.connect()
-        if retry_until_valid:
-            value = super().get(count, as_string, as_numpy, timeout,
-                                with_ctrlvars, use_monitor)
-            while value is None:
-                value = super().get(count, as_string, as_numpy, timeout,
-                                    with_ctrlvars, use_monitor)
-                print(f"{self.pvname} value is None, retrying")
-                sleep(0.5)
-            return value
+        if use_caget:
+            self.caget()
         
         else:
-            return super().get(count, as_string, as_numpy, timeout,
-                               with_ctrlvars, use_monitor)
+            self.connect()
+            
+            value = super().get(count, as_string, as_numpy, timeout,
+                                with_ctrlvars, use_monitor)
+            if value is not None:
+                return value
+            else:
+                return self.caget()
     
     def put(self, value, wait=True, timeout=30.0,
             use_complete=False, callback=None, callback_data=None,
             waitForPut=False):
-        super(PV, self).put(value, wait=wait, timeout=timeout,
-                            use_complete=use_complete, callback=callback,
-                            callback_data=callback_data)
         
-        if waitForPut:
-            attempt = 1
-            while self.severity == EPICS_INVALID_VAL or not self.connect():
-                if attempt >= 5:
-                    raise PVInvalidError("{pv} invalid or disconnected, aborting wait for put"
-                                         .format(pv=self.pvname))
-                attempt += 1
-                sleep(0.1)
-            
-            if type(value) != float:
-                while self.value != value:
-                    self.printAndSleep(value)
-            else:
-                while not isclose(self.value, value, rel_tol=1e-06):
-                    self.printAndSleep(value)
-    
-    def printAndSleep(self, value):
-        print("waiting for {pv} to be {val} at {time}".format(pv=self.pvname,
-                                                              val=value,
-                                                              time=datetime.now()))
-        sleep(1)
+        status = super().put(value, wait=wait, timeout=timeout,
+                             use_complete=use_complete, callback=callback,
+                             callback_data=callback_data)
+        
+        if status is not 1:
+            self.caput(value)
