5ec672894cf8593e911ecf44376fb1f9b06f8292
MattKing06 <matthew.king@stfc.ac.uk>
2023-10-04 19:36:17 +0100

    reformatted

--- lcls_tools/common/devices/profile_monitor/profile_monitor_test.py
+++ lcls_tools/common/devices/profile_monitor/profile_monitor_test.py
@@ -9,42 +9,38 @@ from profile_monitor import ProfMon, get_profile_monitors
 
 
 PROF = {
-    'set': 'test:PNEUMATIC',
-    'get': 'test:TGT_STS',
-    'image': 'test:IMAGE',
-    'res': 'test:RESOLUTION',
-    'xsize': 'test:N_OF_COL',
-    'ysize': 'test:N_OF_ROW',
-    'rate': 'test:FRAME_RATE',
+    "set": "test:PNEUMATIC",
+    "get": "test:TGT_STS",
+    "image": "test:IMAGE",
+    "res": "test:RESOLUTION",
+    "xsize": "test:N_OF_COL",
+    "ysize": "test:N_OF_ROW",
+    "rate": "test:FRAME_RATE",
 }
 
 PROF2 = {
-    'set': 'test:PNEUMATIC',
-    'get': 'test:TGT_STS',
-    'image': 'test:Image:ArrayData',
-    'res': 'test:RESOLUTION',
-    'xsize': 'test:ArraySizeX_RBV',
-    'ysize': 'test:ArraySizeY_RBV',
-    'rate': 'test:FRAME_RATE',
+    "set": "test:PNEUMATIC",
+    "get": "test:TGT_STS",
+    "image": "test:Image:ArrayData",
+    "res": "test:RESOLUTION",
+    "xsize": "test:ArraySizeX_RBV",
+    "ysize": "test:ArraySizeY_RBV",
+    "rate": "test:FRAME_RATE",
 }
 
-PROFS = [
-    'OTR02',
-    'YAG01B',
-    'YAG01'
-]
+PROFS = ["OTR02", "YAG01B", "YAG01"]
+
 
 class ProfileMonitorTest(unittest.TestCase):
-    
     ############ Constants ############
 
     def test_create_profmon_dict(self):
         """Typical LCLS style PV naming convention"""
-        self.assertEqual(pc.create_profmon_dict('test'), PROF)
+        self.assertEqual(pc.create_profmon_dict("test"), PROF)
 
     def test_create_profmon2_dict(self):
         """New LCLS2 style PV naming convention"""
-        self.assertEqual(pc.create_profmon2_dict('test'), PROF2)
+        self.assertEqual(pc.create_profmon2_dict("test"), PROF2)
 
     def test_get_profile_monitors(self):
         """To verify we haven't added any profs"""
@@ -77,9 +73,10 @@ class ProfileMonitorTest(unittest.TestCase):
     def test_name(self):
         """Test that we get default name and can hand name in init arg"""
         p = ProfMon()
-        self.assertEqual(p.prof_name, 'OTR02')
-        p = ProfMon('YAG01')
-        self.assertEqual(p.prof_name, 'YAG01')
+        self.assertEqual(p.prof_name, "OTR02")
+        p = ProfMon("YAG01")
+        self.assertEqual(p.prof_name, "YAG01")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
