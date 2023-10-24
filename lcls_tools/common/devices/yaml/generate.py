62b7f9accecc3df39b661b995ff7d29708f089fd
matt <matt@SLACMachine.myguest.virtualbox.org>
2023-10-24 02:09:53 +0100

    restrict wildcard search in meme

--- lcls_tools/common/devices/yaml/generate.py
+++ lcls_tools/common/devices/yaml/generate.py
@@ -63,7 +63,7 @@ class YAMLGenerator:
         for search_term in search_list:
             # End of the PV name is implied in search_term
             try:
-                pv_list = meme.names.list_pvs(name + "%" + search_term, sort_by="z")
+                pv_list = meme.names.list_pvs(name + ":" + search_term, sort_by="z")
                 if pv_list != list():
                     pv = pv_list[0]
                     handle = pv.split(":")[-1].lower()
