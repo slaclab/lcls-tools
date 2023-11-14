6b323caf2ab40becca8ae97411d99dd194727b53
Neveu <nneveu@stanford.edu>
2023-11-14 15:04:53 -0800

    updating yaml files to have unified keys

---
+++ lcls_tools/common/devices/reader.py
@@ -0,0 +1,49 @@
+import os
+import yaml
+from typing import Union
+from pydantic import ValidationError
+from lcls_tools.common.devices.magnet.magnet import Magnet, MagnetCollection
+
+DEFAULT_YAML_LOCATION = "./lcls_tools/common/devices/yaml/"
+
+
+def _find_yaml_file(area: str) -> str:
+    filename = area + ".yaml"
+    path = os.path.join(DEFAULT_YAML_LOCATION, filename)
+    if os.path.isfile(path):
+        return os.path.abspath(path)
+    else:
+        raise FileNotFoundError(
+            f"No such file {path}, please choose another area.",
+        )
+
+
+def create_magnet(
+    area: str = None, name: str = None
+) -> Union[None, Magnet, MagnetCollection]:
+    if area:
+        try:
+            location = _find_yaml_file(
+                area=area,
+            )
+            with open(location, "r") as device_file:
+                config_data = yaml.safe_load(device_file)
+                if name:
+                    magnet_data = config_data["magnets"][name]
+                    # this data is not available from YAML directly in this form, so we add it here.
+                    magnet_data.update({"name": name})
+                    return Magnet(**magnet_data)
+                else:
+                    return MagnetCollection(**config_data)
+        except FileNotFoundError:
+            print(f"Could not find yaml file for area: {area}")
+            return None
+        except KeyError:
+            print(f"Could not find name {name} in file for area: {area}")
+            return None
+        except ValidationError as field_error:
+            print(field_error)
+            return None
+    else:
+        print("Please provide a machine area to create a magnet from.")
+        return None
