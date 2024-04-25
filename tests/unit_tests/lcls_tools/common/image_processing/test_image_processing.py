44223a30198e844a1664988757b62de6b9078e8f
Chris Garnier <cgarnier@slac.stanford.edu>
2024-04-25 15:39:59 -0700

    updating unittests for test_image_processing and test_roi, still not quite done

--- tests/unit_tests/lcls_tools/common/image_processing/test_image_processing.py
+++ tests/unit_tests/lcls_tools/common/image_processing/test_image_processing.py
@@ -4,14 +4,37 @@ import numpy as np
 import lcls_tools.common.image_processing as ImageProcessor
 
 class TestImageCreator():
-    pass
+    def create_test_image(self, size: tuple, center: list, radius: int):
+        ''' 
+        make img that is a circle in the center of the image with known
+        standard dev and mean. no imports, no calls to external or
+        internal files.
+         '''
+        image = np.zeros(size)
+        for y in range(image.shape[0]):
+            for x in range(image.shape[1]):
+                distance = np.sqrt((x - center[0]) ** 2 + (y - center[1]) ** 2)
+                if distance < radius:
+                    image[y, x] = 1
+        return image
+
 
 class ImageProcessingTest(unittest.TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
         super().__init__(methodName)
+        self.center = [400,400]
+        self.size = (800,800)
+        self.widths= (300,300)
+        self.radius = 50
+        self.image_creator = TestImageCreator()
+        self.image = self.image_creator.create_test_image(size = self.size, center=self.center,radius = self.radius)
     def test_subtract_background(self):
-        pass
+        image_processor = ImageProcessor()
+        # need to create a background image
+        image = image_processor.subtract_background(self.image)
+        # hm?
     def test_process(self):
-        pass 
-
+        image_processor = ImageProcessor()
+        image = image_processor.process(self.image)
+        # hm? 
 
--- tests/unit_tests/lcls_tools/common/image_processing/test_roi.py
+++ tests/unit_tests/lcls_tools/common/image_processing/test_roi.py
@@ -1,17 +1,49 @@
 import unittest
 import os
 import numpy as np
-import lcls_tools.common.roi as ROI,RectangularROI,CircularROI
+from lcls_tools.common.image_processing.roi import ROI,RectangularROI,CircularROI
 
-class TestImageCreator():
-    pass
+class TestImageCreator():   
+    def create_test_image(self, size: tuple, center: list, radius: int):
+        ''' 
+        make img that is a circle in the center of the image with known
+        standard dev and mean. no imports, no calls to external or
+        internal files.
+         '''
+        image = np.zeros(size)
+        for y in range(image.shape[0]):
+            for x in range(image.shape[1]):
+                distance = np.sqrt((x - center[0]) ** 2 + (y - center[1]) ** 2)
+                if distance < radius:
+                    image[y, x] = 1
+        return image
 
-class ImageProcessingTest(unittest.TestCase):
+class ROITest(unittest.TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
         super().__init__(methodName)
+        self.center = [400,400]
+        self.size = (800,800)
+        self.widths= (300,300)
+        self.radius = 50
+        self.image_creator = TestImageCreator()
+        self.image = self.image_creator.create_test_image(size = self.size, center=self.center,radius = self.radius)
+    def test_roi(self):
+        rectangular =  RectangularROI(center =self.center,xwidth=self.widths[0], ywidth=self.widths[1])
+        circular = CircularROI(center =self.center,radius =self.radius)
+
     def test_circular_roi_crop_image(self):
-        pass
+        circular = CircularROI(center =self.center,radius = self.radius)
+        cropped_image = circular.crop_image(self.image)
+        assert cropped_image.shape[0] < 200
+        assert cropped_image.shape[1] < 200
+        # print idk.... this one is annoying.
+
     def test_fill_value_outside_circle(self):
+        #print also annoying...
+        
         pass
+
     def test_rectangular_roi_crop_image(self):
-        pass
+        rectangular =  RectangularROI(center =self.center,xwidth=self.widths[0], ywidth=self.widths[1])
+        cropped_image = rectangular.crop_image(self.image)
+        assert cropped_image.shape == self.widths
