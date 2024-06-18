74bc1b9a072c3f4137f94a1670813f90c282a309
Eloise Yang <98988862+eloiseyang@users.noreply.github.com>
2024-06-18 15:09:47 -0700

    roi.py refactored to introduce code sharing roi.py added EllipticalROI

--- lcls_tools/common/image/roi.py
+++ lcls_tools/common/image/roi.py
@@ -1,109 +1,90 @@
 from abc import ABC, abstractmethod
-from typing import (
-    List,
-)
 import numpy as np
-from matplotlib import patches
-from pydantic import BaseModel, PositiveFloat, Field
-
+from pydantic import BaseModel, PositiveFloat
+from typing import ClassVar, List, Optional
 
 class ROI(BaseModel, ABC):
-    roi_type: str
     center: List[PositiveFloat]
 
+    @property
     @abstractmethod
-    def crop_image(self, img, **kwargs) -> np.ndarray:
-        """crop image using ROI"""
+    def bounds(self):
+        """Abstract proprety for the bounding width."""
         pass
-
-    @abstractmethod
-    def get_patch(self):
-        pass
-
-
-class CircularROI(ROI):
-    """
-    Define a circular region of interest (ROI) for an image, cropping pixels outside a
-    bounding box around the ROI and setting pixels outside the boundary to a fill
-    value (usually zero).
-    """
-
-    roi_type: str = Field(default="circular", frozen=True)
-    radius: PositiveFloat
 
     @property
-    def bounding_box(self):
+    def box(self):
         return [
-            int(self.center[0] - int(self.radius)),
-            int(self.center[1] - int(self.radius)),
-            int(self.radius * 2),
-            int(self.radius * 2),
+            int(self.center[0] - int(self.bounds[0] / 2)),
+            int(self.center[1] - int(self.bounds[1] / 2)),
+            int(self.center[0] + int(self.bounds[0] / 2)),
+            int(self.center[1] + int(self.bounds[1] / 2))
         ]
 
-    def crop_image(self, img, **kwargs) -> np.ndarray:
-        fill_value = kwargs.get("fill_value", 0.0)
-        img = self.fill_value_outside_circle(img, self.center, self.radius, fill_value)
-        bbox = self.bounding_box
-        img = img[bbox[1]: bbox[1] + bbox[3], bbox[0]: bbox[0] + bbox[2]]
+    def crop_image(self, img) -> np.ndarray:
+        """Crop image using the ROI center and bounding width."""
+        x_size, y_size = img.shape
+        if self.bounds[0] > x_size or self.bounds[1] > y_size:
+            raise ValueError(
+                f"must pass image that is larger than ROI, "
+                f"image size is {img.shape}, "
+            )
+        img = img[self.box[0]:self.box[2],
+                  self.box[1]:self.box[3]]
         return img
-
-    def fill_value_outside_circle(
-        self,
-        img: np.ndarray,
-        center: List[PositiveFloat],
-        radius: PositiveFloat,
-        fill_value: float,
-    ):
-        height, width = img.shape
-        for y in range(height):
-            for x in range(width):
-                distance = np.sqrt((x - center[0]) ** 2 + (y - center[1]) ** 2)
-                if distance > radius:
-                    img[y, x] = fill_value
-        return img
-
-    def get_patch(self):
-        return patches.Circle(
-            tuple(self.center), self.radius, facecolor="none", edgecolor="r"
-        )
-
 
 class RectangularROI(ROI):
     """
     Define a rectangular region of interest (ROI) for an image, cropping pixels outside
     the ROI.
     """
-
-    # set roi_type : str = 'rectangular' and freeze it.
-    roi_type: str = Field(default="rectangular", frozen=True)
-    xwidth: int
-    ywidth: int
+    width: List[PositiveFloat]
 
     @property
-    def bounding_box(self):
-        return [
-            int(self.center[0] - int(self.xwidth / 2)),
-            int(self.center[1] - int(self.ywidth / 2)),
-            int(self.xwidth),
-            int(self.ywidth),
-        ]
+    def bounds(self):
+        return self.width
 
-    def crop_image(self, img, **kwargs) -> np.ndarray:
-        x_size, y_size = img.shape
-        if self.xwidth > x_size or self.ywidth > y_size:
-            raise ValueError(
-                f"must specify ROI that is smaller than the image, "
-                f"image size is {img.shape}"
-            )
-        bbox = self.bounding_box
-        img = img[bbox[1]: bbox[1] + bbox[3], bbox[0]: bbox[0] + bbox[2]]
+class EllipticalROI(ROI):
+    """
+    Define an elliptical region of interest (ROI) for an image.
+    """
+    radius: List[PositiveFloat]
+
+    @property
+    def bounds(self):
+        return [r * 2 for r in self.radius]
+
+    def negative_fill(self, img, fill_value):
+        """ Fill the region outside the defined ellipse. """
+        r = self.radius
+        if type(r) is float:
+            r = [r, r]
+        c = self.center
+        height, width = img.shape
+        for y in range(height):
+            for x in range(width):
+                distance = (((x - c[0]) / r[0]) ** 2 +
+                            ((y - c[1]) / r[1]) ** 2)
+                if distance > 1:
+                    img[y, x] = fill_value
         return img
 
-    def get_patch(self):
-        return patches.Rectangle(
-            (self.bounding_box[0], self.bounding_box[1]),
-            self.bounding_box[2],
-            self.bounding_box[3],
-            facecolor="none",
-            edgecolor="r",
-        )
+    def crop_image(self, img, **kwargs) -> np.ndarray:
+        """
+        Crop the pixels outside a bounding box and set the boundary to a fill
+        value (usually zero).
+        """
+        img = super().crop_image(img)
+        fill_value = kwargs.get("fill_value", 0.0)
+        img = self.negative_fill(img, fill_value)
+        return img
+
+class CircularROI(EllipticalROI):
+    """
+    Define a circular region of interest (ROI) for an image.
+    """
+    radius: PositiveFloat
+
+    @property
+    def bounds(self):
+        return [self.radius * 2, self.radius * 2]
