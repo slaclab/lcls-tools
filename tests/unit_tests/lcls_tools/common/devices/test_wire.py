e1140a1c4cb72d1d66b86e36d5618686843b83ef
kabanaty <tylerkabana@gmail.com>
2024-08-01 14:48:26 -0700

    final flake8???

--- tests/unit_tests/lcls_tools/common/devices/test_wire.py
+++ tests/unit_tests/lcls_tools/common/devices/test_wire.py
@@ -3,7 +3,7 @@
 # Built in
 # import datetime
 from unittest import TestCase
-from unittest.mock import Mock, patch # , PropertyMock
+from unittest.mock import Mock, patch  # , PropertyMock
 # import inspect
 
 # Local imports
