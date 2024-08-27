27134d5f86de48dbcc2c52cc374159e764fe1996
Neveu <nneveu@stanford.edu>
2024-08-27 00:18:18 -0700

    tying new yaml import path

--- lcls_tools/common/devices/reader.py
+++ lcls_tools/common/devices/reader.py
@@ -8,8 +8,8 @@ from lcls_tools.common.devices.wire import Wire, WireCollection
 from lcls_tools.common.devices.area import Area
 from lcls_tools.common.devices.beampath import Beampath
 
-
-DEFAULT_YAML_LOCATION = "./lcls_tools/common/devices/yaml/"
+DEFAULT_YAML_LOCATION = os.path.join(os.path.dirname(__file__), 'yaml/')
+#DEFAULT_YAML_LOCATION = "./lcls_tools/common/devices/yaml/"
 
 
 def _find_yaml_file(
