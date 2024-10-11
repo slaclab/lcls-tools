f10801abbb5b38d08ab041e2e9233f80c59e8cf4
Shamin Chowdhury <shamin@slac.stanford.edu>
2024-10-10 20:32:54 -0700

    - Make fields like model and magnet_collection required - Replace device attribute with device_measurement - Replace get_beamsize() method with device_measurement.measure() method

--- lcls_tools/common/measurements/emittance_measurement.py
+++ lcls_tools/common/measurements/emittance_measurement.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+from pydantic import Field
 from lcls_tools.common.devices.reader import create_magnet, create_screen, create_wire
 from lcls_tools.common.devices.magnet import MagnetCollection
 from lcls_tools.common.devices.device import Device
@@ -6,60 +9,42 @@ from lcls_tools.common.measurements.measurement import Measurement
 from meme.model import Model
 
 class QuadScanEmittance(Measurement):
-    model_beamline: str
-    magnet_area: str
+    model: Model
+    magnet_collection: MagnetCollection
     magnet_name: str
     scan_values: list[float]
-    rmats: list
-    _model: Model = None
-    _magnet_collection: MagnetCollection = None
-    _magnet_settings: list[dict] = None
-
-    to_device_area: str
-    to_device_name: str
-    beam_sizes: list
-    _to_device: Device = None
+    _magnet_settings: list[dict] # example: [{"QE04": -6}, {"QE04": -3}, {"QE04": 0}]
+    device_measurement: Measurement
+    _rmats: list
+    _beam_sizes: dict
 
     def __init__(self):
         super().__init__()
 
     @property
-    def model(self) -> Model:
-        if self._model is None:
-            self._model = Model(self.model_beamline)
-        return self._model
-    
-    @property
-    def magnet_collection(self) -> MagnetCollection:
-        if self._magnet_collection is None:
-            self._magnet_collection = create_magnet(area=self.magnet_area)
-        return self._magnet_collection
-    
-    @property
     def magnet_settings(self) -> list[dict]:
         if self._magnet_settings is None:
             self._magnet_settings = [{self.magnet_name:value} for value in self.scan_values]
         return self._magnet_settings
-    
-    @property
-    def to_device(self) -> Device:
-        if self._to_device is None:
-            if self.to_device_name.startswith('OTR','YAG'):
-                self.to_device = create_screen(area=self.to_device_area, name=self.to_device_name)
-            else:
-                self.to_device = create_wire(area=self.to_device_area, name=self.to_device_name)
 
     def measure(self):
+        self._rmats.append(self.model.get_rmat(from_device=self.magnet_name, to_device=self.device_measurement.device.name))
         self.magnet_collection.scan(scan_settings=self.magnet_settings, function=self.measure_beamsize)
+        beamsize_squared = np.vstack((self._beam_sizes["x_rms"], self._beam_sizes["y_rms"]))**2
+        magnet_length = self.magnet_collection.magnets[self.magnet_name].length
+        twiss = self.model.get_twiss(self.magnet_name)
+        twiss_betas_alphas = np.array([[twiss["beta_x"],twiss["alpha_x"]],[twiss["beta_y"],twiss["alpha_y"]]])
 
-        emittance, bmag, sig, is_valid = compute_emit_bmag(
-                                            k = self.scan_values,
-                                            beamsize_squared = self.beam_sizes, # [beam_sizes['x_rms'],beam_sizes['y_rms']],
-                                            q_len = 0.221, # self.to_device magnet length?
-                                            rmat = self.rmats,
-                                            beta0 = 0.0001, # ?
-                                            alpha0 = 0.0002, # ?
-                                            get_bmag = True)
+
+        emittance, bmag, _, _ = compute_emit_bmag(
+            k = self.scan_values,
+            beamsize_squared = beamsize_squared,
+            q_len = magnet_length,
+            rmat = self._rmats,
+            twiss_design = twiss_betas_alphas,
+            # thin_lens = ,
+            # maxiter = 
+        )
 
         results = {
             "emittance": emittance,
@@ -69,11 +54,16 @@ class QuadScanEmittance(Measurement):
         return results
     
     def measure_beamsize(self):
-        self.beam_sizes.append(self.to_device.get_beamsize())
-        self.rmats.append(self.model.get_rmat(from_device=self.magnet_collection.name, to_device=self.to_device.name))
+        results = self.device_measurement.measure()
+        if "x_rms" not in self._beam_sizes:
+            self._beam_sizes["x_rms"] = []
+        if "y_rms" not in self._beam_sizes:
+            self._beam_sizes["y_rms"] = []
+        self._beam_sizes["x_rms"].append(results["Sx"])
+        self._beam_sizes["y_rms"].append(results["Sy"])
     
 class MultiDeviceEmittance(Measurement):
     pass
 
-def compute_emit_bmag(self, k, beamsize_squared, q_len, rmat, beta0, alpha0, get_bmag):
+def compute_emit_bmag(self, k, beamsize_squared, q_len, rmat, twiss_design, thin_lens, maxiter):
     pass                                      
