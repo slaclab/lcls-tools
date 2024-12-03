7f75da632c5d3dd1440fe3350539a8d915114bd6
Ryan Roussel <rroussel@slac.stanford.edu>
2024-12-03 16:47:03 -0600

    Update test_sc_cryomodule.py

--- tests/unit_tests/lcls_tools/superconducting/test_sc_cryomodule.py
+++ tests/unit_tests/lcls_tools/superconducting/test_sc_cryomodule.py
@@ -12,7 +12,7 @@ class TestCryomodule(TestCase):
         )
 
     def test_is_harmonic_linearizer_false(self):
-        cm = MACHINE.cryomodules[f"{randint(1, 35): 02d}"]
+        cm = MACHINE.cryomodules[f"{randint(1, 35):02d}"]
         self.assertFalse(
             cm.is_harmonic_linearizer, msg=f"{cm} is_harmonic_linearizer is not false"
         )
@@ -22,5 +22,5 @@ class TestCryomodule(TestCase):
         self.assertEqual(cm.pv_prefix, "ACCL:L0B:0100:")
 
     def test_num_cavities(self):
-        cm = MACHINE.cryomodules[f"{randint(1, 35): 02d}"]
+        cm = MACHINE.cryomodules[f"{randint(1, 35):02d}"]
         self.assertEqual(len(cm.cavities.keys()), 8)
