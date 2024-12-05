e78dbc11f83d40d414ca9fefbfd99a94e3c3d44d
Chris Garnier <cgarnier@slac.stanford.edu>
2024-12-05 15:22:23 -0800

    adapted magnet.py and emittance_measurement.py by to use new metadata field corresponding to magnetic length : l_eff

--- lcls_tools/common/devices/magnet.py
+++ lcls_tools/common/devices/magnet.py
@@ -59,7 +59,7 @@ class MagnetControlInformation(ControlInformation):
 
 
 class MagnetMetadata(Metadata):
-    length: Optional[PositiveFloat] = None
+    l_eff: Optional[PositiveFloat] = None
     b_tolerance: Optional[PositiveFloat] = None
 
     def __init__(self, *args, **kwargs):
@@ -123,15 +123,15 @@ class Magnet(Device):
         self.metadata.b_tolerance = value
 
     @property
-    def length(self):
+    def l_eff(self):
         """Returns the effective length in meters"""
-        return self.metadata.length
+        return self.metadata.l_eff
 
-    @length.setter
-    def length(self, value):
+    @l_eff.setter
+    def l_eff(self, value):
         if not isinstance(value, float):
             return
-        self.metadata.length = value
+        self.metadata.l_eff = value
 
     @property
     def bctrl(self) -> Union[float, int]:
