4f0d78ed887d2eb770a7d9de863624da53c22e2b
Sara A. Miskovich <saramiskovich@gmail.com>
2025-01-21 14:51:07 -0800

    udpate packaging and workflows

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="0.1.dev2",
     url="https://github.com/slaclab/lcls-tools",
     license="Apache License",
-    python_requires=">=3.6",
+    python_requires=">=3.9",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     include_package_data=True,
