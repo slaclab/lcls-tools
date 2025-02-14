aab01814b7b5e09ee4fe574a5539b3463ec98ba3
Ryan Roussel <rroussel@slac.stanford.edu>
2025-02-14 14:42:25 -0600

    update measurement classes to return structured dataframes

--- lcls_tools/common/measurements/screen_profile.py
+++ lcls_tools/common/measurements/screen_profile.py
@@ -1,9 +1,49 @@
+import numpy as np
+from typing import Any
+
 from lcls_tools.common.devices.screen import Screen
 from lcls_tools.common.image.fit import ImageProjectionFit, ImageFit
 from lcls_tools.common.image.processing import ImageProcessor
 from lcls_tools.common.measurements.measurement import Measurement
-from pydantic import ConfigDict
+from pydantic import BaseModel, ConfigDict, SerializeAsAny, SkipValidation, field_validator
 from typing import Optional
+
+
+class ScreenBeamProfileMeasurementResult(BaseModel):
+    """
+    Class that contains the results of a beam profile measurement
+
+    Attributes
+    ----------
+    raw_images : ndarray
+        Numpy array of raw images taken during the measurement
+    processed_images : ndarray
+        Numpy array of processed images taken during the measurement
+    rms_sizes : ndarray
+        Numpy array of rms sizes of the beam in pixel units.
+    centroids : ndarray
+        Numpy array of centroids of the beam in pixel units.
+    total_intensities : ndarray
+        Numpy array of total intensities of the beam.
+    metadata : Any
+        Metadata information related to the measurement.
+
+    """
+    raw_images: np.ndarray
+    processed_images: np.ndarray
+    rms_sizes: Optional[np.ndarray] = None
+    centroids: Optional[np.ndarray] = None
+    total_intensities: Optional[np.ndarray] = None
+    metadata: SkipValidation[SerializeAsAny[Any]]
+
+    model_config = ConfigDict(arbitrary_types_allowed=True, extra="forbid")
+
+    @field_validator("*", mode="before")
+    def validate_numpy_array(cls, v, field):
+        if v is not None:
+            if not isinstance(v, np.ndarray):
+                v = np.array(v)
+        return v
 
 
 class ScreenBeamProfileMeasurement(Measurement):
@@ -46,14 +86,24 @@ class ScreenBeamProfileMeasurement(Measurement):
             images.append(self.device.image)
             # TODO: need to add a wait statement in here for images to update
 
-        results = {"raw_images": images, "fit_results": None}
+        processed_images = [self.image_processor.auto_process(image) for image in images]
+
 
         if self.fit_profile:
-            fit_results = []
-            for image in images:
-                processed_image = self.image_processor.auto_process(image)
-                fit_results += [self.beam_fit.fit_image(processed_image)]
+            rms_sizes = []
+            centroids = []
+            total_intensities = []
+            for image in processed_images:
+                fit_result = self.beam_fit.fit_image(image)
+                rms_sizes.append(fit_result.rms_size)
+                centroids.append(fit_result.centroid)
+                total_intensities.append(fit_result.total_intensity)
 
-            results["fit_results"] = fit_results
-
-        return results
+        return ScreenBeamProfileMeasurementResult(
+            raw_images=images,
+            processed_images=processed_images,
+            rms_sizes=rms_sizes or None,
+            centroids=centroids or None,
+            total_intensities=total_intensities or None,
+            metadata=self.model_dump(),
+        )
