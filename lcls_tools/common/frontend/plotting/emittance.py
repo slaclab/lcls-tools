80a48149985cbcc50d2d478eff16c4263b76235e
Ryan Roussel <rroussel@slac.stanford.edu>
2025-02-19 17:24:37 -0600

    refactor emittance measurement calculations to allow for any number of nan values

--- lcls_tools/common/frontend/plotting/emittance.py
+++ lcls_tools/common/frontend/plotting/emittance.py
@@ -22,47 +22,40 @@ def plot_quad_scan_result(emittance_results):
     fig, ax = plt.subplots(2, 1, sharex=True)
     fig.set_size_inches(4, 6)
 
-    sorted_indices = np.argsort(emittance_results.quadrupole_focusing_strengths)
-    k = emittance_results.quadrupole_focusing_strengths[sorted_indices]
-    beta = emittance_results.twiss_at_screen.transpose(1, 0, 2)[sorted_indices][..., 0]
+    c = ["x", "y"]
+    for i in range(2):
+        sorted_indices = np.argsort(emittance_results.quadrupole_pv_values[i])
+        k = emittance_results.quadrupole_pv_values[i][sorted_indices]
+        beta = emittance_results.twiss_at_screen[i][sorted_indices][:, 0]
 
-    ax[0].plot(k, emittance_results.rms_x[sorted_indices] * 1e6, "+", label="x_rms")
-    ax[0].plot(k, emittance_results.rms_y[sorted_indices] * 1e6, "+", label="y_rms")
+        ax[0].plot(k, emittance_results.rms_beamsizes[i][sorted_indices] * 1e6, "+", label=f"rms_{c[i]}")
 
-    # plot fit from twiss at screen calculation
-    ax[0].plot(
-        k,
-        np.sqrt(beta[:, 0] * emittance_results.emittance[0]) * 1e3,
-        "--",
-        label="x_fit",
-    )
-    ax[0].plot(
-        k,
-        np.sqrt(beta[:, 1] * emittance_results.emittance[1]) * 1e3,
-        "--",
-        label="y_fit",
-    )
-
-    # ax[0].set_xlabel("Quadrupole Strength [T/m]")
-    ax[0].set_ylabel("Beam size [um]")
-
-    if emittance_results.bmag is not None:
-        ax[1].plot(k, emittance_results.bmag[0][sorted_indices], "+", label="bmag x")
-        ax[1].plot(k, emittance_results.bmag[1][sorted_indices], "+", label="bmag y")
-
-        ax[1].set_xlabel("Quadrupole Strength [T/m]")
-        ax[1].set_ylabel("bmag")
-        ax[1].axhline(1.0, color="black", linestyle="--")
-    else:
-        # add text to the middle of the axis that says "BMAG not available"
-        ax[1].text(
-            0.5,
-            0.5,
-            "bmag not available",
-            ha="center",
-            va="center",
-            transform=ax[1].transAxes,
+        # plot fit from twiss at screen calculation
+        ax[0].plot(
+            k,
+            np.sqrt(beta * emittance_results.emittance[i]) * 1e3,
+            "--",
+            label=f"{c[i]}_fit",
         )
+
+        if emittance_results.bmag is not None:
+            ax[1].plot(k, emittance_results.bmag[i][sorted_indices], "+", label=f"bmag {c[i]}")
+            ax[1].set_xlabel("Quadrupole Strength [T/m]")
+            ax[1].set_ylabel("bmag")
+            ax[1].axhline(1.0, color="black", linestyle="--")
+        else:
+            # add text to the middle of the axis that says "BMAG not available"
+            ax[1].text(
+                0.5,
+                0.5,
+                "bmag not available",
+                ha="center",
+                va="center",
+                transform=ax[1].transAxes,
+            )
+
+    ax[0].set_xlabel("Quadrupole Strength [T/m]")
+    ax[0].set_ylabel("Beam size [um]")
 
     for ele in ax:
         ele.legend()
