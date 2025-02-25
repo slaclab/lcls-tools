c1ea061c4317da41d1566e19ef3ba142605afd08
Neveu <nneveu@stanford.edu>
2025-02-25 10:30:37 -0800

    removing fitting_tool from docs again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,6 @@ import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(__file__, "../../../")))
-<<<<<<< HEAD
 from lcls_tools.common.devices.magnet import *
 from lcls_tools.common.devices.beampath import *
 from lcls_tools.common.devices.device import *
@@ -15,16 +14,6 @@ from lcls_tools.common.matlab2py.mat_image import *
 from lcls_tools.common.matlab2py.mat_emit_scan import *
 from lcls_tools.common.matlab2py.mat_corr_plot import *
 =======
-from lcls_tools.common.data.fitting_tool import *  # noqa: F403
-from lcls_tools.common.devices.magnet import *  # noqa: F403
-from lcls_tools.common.devices.beampath import *  # noqa: F403
-from lcls_tools.common.devices.device import *  # noqa: F403
-from lcls_tools.common.devices.screen import *  # noqa: F403
-from lcls_tools.common.matlab2py.mat_image import *  # noqa: F403
-from lcls_tools.common.matlab2py.mat_emit_scan import *  # noqa: F403
-from lcls_tools.common.matlab2py.mat_corr_plot import *  # noqa: F403
->>>>>>> a147956 (rebase and format)
-
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
