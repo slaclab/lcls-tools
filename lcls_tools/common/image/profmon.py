284ff7bbd08fe42daaba1bf176d6cf446d667ab3
Eloise Fae Yang <98988862+eloiseyang@users.noreply.github.com>
2025-04-14 11:21:49 -0700

    Forgot some arguments in lcls_tools.common.image.profmon

--- lcls_tools/common/image/profmon.py
+++ lcls_tools/common/image/profmon.py
@@ -9,23 +9,23 @@ curve_dict = {
 }
 
 
-def profile_stat(profile, curve="gaussian"):
+def profile_stat(x, profile, curve="gaussian"):
     fit = curve_dict[curve]
-    params = fit(profile)
+    params = fit(x, profile)
     stat = {"centroid": params["mu"], "rms_size": params["sigma"]}
     return stat
 
 
-def image_stat(image, curve="gaussian"):
+def image_stat(image, x, y, curve="gaussian"):
     x_projection = numpy.array(numpy.sum(image, axis=0))
     y_projection = numpy.array(numpy.sum(image, axis=1))
-    x_stat = profile_stat(x_projection, curve=curve)
-    y_stat = profile_stat(y_projection, curve=curve)
+    x_stat = profile_stat(x, x_projection, curve=curve)
+    y_stat = profile_stat(y, y_projection, curve=curve)
     centroid = [x_stat["centroid"], y_stat["centroid"]]
     rms_size = [x_stat["rms_size"], y_stat["rms_size"]]
     stat = {
         "centroid": centroid,
         "rms_size": rms_size,
-        "intensity": image.sum(),
+        "total_intensity": image.sum(),
     }
     return stat
