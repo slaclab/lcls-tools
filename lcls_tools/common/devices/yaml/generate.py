9d3e1790dc30b0ccebf0bb6481028eeeaf810263
chris garnier <cgarnier@slac.stanford.edu>
2025-04-16 14:30:15 -0700

    linter

--- lcls_tools/common/devices/yaml/generate.py
+++ lcls_tools/common/devices/yaml/generate.py
@@ -318,7 +318,7 @@ class YAMLGenerator:
             "N_OF_BITS": "n_bits",
             "SYS_TYPE": "sys_type",
             "FRAME_RATE": "ref_rate_vme",
-            "ArrayRate_RBV": "ref_rate"
+            "ArrayRate_RBV": "ref_rate",
         }
         # should be structured {MAD-NAME : {field_name : value, field_name_2 : value}, ... }
         additional_metadata_data = get_screen_metadata()
