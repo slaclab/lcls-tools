910043e665272934ca3242522d7eaa1659f06f27
chris garnier <cgarnier@slac.stanford.edu>
2025-04-29 15:29:07 -0700

    updated tcav.py to reflect requested changes in PR review

--- lcls_tools/common/devices/tcav.py
+++ lcls_tools/common/devices/tcav.py
@@ -1,6 +1,4 @@
-from functools import wraps
 from pydantic import (
-    Field,
     NonNegativeFloat,
     SerializeAsAny,
     field_validator,
@@ -8,13 +6,10 @@ from pydantic import (
 from typing import (
     Dict,
     Optional,
-    Union,
-    List,
 )
 from lcls_tools.common.devices.device import (
     Device,
     ControlInformation,
-    DeviceCollection,
     Metadata,
     PVSet,
 )
@@ -31,9 +26,6 @@ class TCAVPVSet(PVSet):
     phase_fbst: PV
     mode_config: PV
 
-    def __init__(self, *args, **kwargs):
-        super(TCAVPVSet, self).__init__(*args, **kwargs)
-
     @field_validator("*", mode="before")
     def validate_pv_fields(cls, v: str) -> PV:
         return PV(v)
@@ -42,72 +34,116 @@ class TCAVPVSet(PVSet):
 class TCAVControlInformation(ControlInformation):
     PVs: SerializeAsAny[TCAVPVSet]
     _mode_config_options: SerializeAsAny[Optional[Dict[str, int]]] = dict()
+    _amplitude_feedback_options: SerializeAsAny[Optional[Dict[str, int]]] = dict()
+    _phase_feedback_options: SerializeAsAny[Optional[Dict[str, int]]] = dict()
 
-    def __init__(self, *args, **kwargs):
-        super(TCAVControlInformation, self).__init__(*args, **kwargs)
+    def model_post_init(self, __context) -> None:
+        """
+        Post-initialization hook for Pydantic models.
+        Retrieves and stores all PV enum options immediately after model creation.
+        Args:
+            __context: Reserved for Pydantic internals. Must be present for compliance.
+        Raises:
+            TimeoutError: If any PV fails to return its control variables.
+        """
+        _ = __context  # avoid linter warning for unused variable
+        self.set_mode_config_option()
+        self.set_amplitude_feedback_options()
+        self.setup_phase_feedback_option()
 
     def set_mode_config_option(self):
-        mode_config_options = self.PVs.mode_configs.get_ctrlvars(timeout=1)
-        if mode_config_options:
-            [
-                self._mode_config_options.update({option: i})
-                for i, option in enumerate(mode_config_options["enum_strs"])
-            ]
+        """
+        Fetches and stores the enumerated options for the mode configuration PV.
+        This method calls `get_ctrlvars()` on the `mode_configs` PV to retrieve
+        its enum string options and populates the `_mode_config_options` dictionary.
+        Raises:
+            TimeoutError: If the PV does not return control variables within the timeout period.
+        """
+        mode_config_options = self.PVs.mode_configs.get_ctrlvars(timeout=2.5)
+        if not mode_config_options:
+            raise TimeoutError(
+                "Timeout while retrieving control variables from mode_configs PV."
+            )
+
+        self._mode_config_options.update(
+            {option: i for i, option in enumerate(mode_config_options["enum_strs"])}
+        )
+
+    def set_amplitude_feedback_options(self):
+        """
+        Fetches and stores the enumerated options for the amplitude feedback enable PV.
+        Retrieves enum strings from the `amp_fbenb` PV using `get_ctrlvars()` and
+        updates `_amplitude_feedback_options`.
+        Raises:
+            TimeoutError: If control variables are not returned within the timeout duration.
+        """
+        amplitude_feedback_options = self.PVs.amp_fbenb.get_ctrlvars(timeout=2.5)
+        if not amplitude_feedback_options:
+            raise TimeoutError(
+                "Timeout while retrieving control variables from amp_fbenb PV."
+            )
+
+        self._amplitude_feedback_options.update(
+            {
+                option: i
+                for i, option in enumerate(amplitude_feedback_options["enum_strs"])
+            }
+        )
+
+    def setup_phase_feedback_option(self):
+        """
+        Fetches and stores the enumerated options for the phase feedback enable PV.
+        Uses `get_ctrlvars()` on the `phase_fbenb` PV to retrieve available options
+        and populates `_phase_feedback_options`.
+        Raises:
+            TimeoutError: If control variables are not available within the timeout window.
+        """
+        phase_feedback_option = self.PVs.phase_fbenb.get_ctrlvars(timeout=2.5)
+        if not phase_feedback_option:
+            raise TimeoutError(
+                "Timeout while retrieving control variables from phase_fbenb PV."
+            )
+
+        self._phase_feedback_options.update(
+            {option: i for i, option in enumerate(phase_feedback_option["enum_strs"])}
+        )
 
     @property
     def mode_config_options(self):
         return self._mode_config_options
+
+    @property
+    def amplitude_feedback_options(self):
+        return self._amplitude_feedback_options
+
+    @property
+    def phase_feedback_options(self):
+        return self._phase_feedback_options
 
 
 class TCAVMetadata(Metadata):
     l_eff: Optional[NonNegativeFloat] = None
     rf_freq: Optional[NonNegativeFloat] = None
 
-    def __init__(self, *args, **kwargs):
-        super(TCAVMetadata, self).__init__(*args, **kwargs)
-
 
 class TCAV(Device):
     controls_information: SerializeAsAny[TCAVControlInformation]
     metadata: SerializeAsAny[TCAVMetadata]
 
-    def __init__(self, *args, **kwargs):
-        super(TCAV, self).__init__(*args, **kwargs)
-
-    def check_options(options_to_check: Union[str, List]):
-        """Decorator to only allow :MODECFG to be set if that option exists for the TCAV"""
-
-        def decorator(function):
-            @wraps(function)
-            def decorated(self, *args, **kwargs):
-                if isinstance(options_to_check, str):
-                    options = [options_to_check]
-                for option in options:
-                    if option not in self.controls_information.mode_config_options:
-                        print(
-                            f"unable to perform process {option} with this TCAV {self.name}"
-                        )
-                        return
-                return function(self, *args, **kwargs)
-
+    # Decorator didn't want to work,  manually controlled types
+    """
+    def validate_enum_value(self, field_options: Dict):
+        def decorator(setter_method: Callable):
+            @wraps(setter_method)
+            def decorated(self, enum_str):
+                if not isinstance(enum_str,str):
+                    raise TypeError(f"{enum_str} is not of type: str")
+                if enum_str not in field_options:
+                    raise ValueError(f"{enum_str} not in list of acceptable enumerate string PV values")
+                return setter_method(self, enum_str)
             return decorated
-
         return decorator
-
-    def check_state(f):
-        """
-        Decorator to enforce that the device is not in 'Disable'
-        mode before executing an operation. Prevents execution of the
-        decorated method if `mode_config == "Disable"`.
-        """
-
-        def decorated(self, *args, **kwargs):
-            if self.mode_config == "Disable":
-                print("Unable to perform action, TCAV is in Disabled state")
-                return
-            return f(self, *args, **kwargs)
-
-        return decorated
+    """
 
     @property
     def amp_set(self):
@@ -115,7 +151,6 @@ class TCAV(Device):
         return self.controls_information.PVs.amp_set.get()
 
     @amp_set.setter
-    @check_state
     def amp_set(self, amplitude):
         if not isinstance(amplitude, float):
             return
@@ -127,7 +162,6 @@ class TCAV(Device):
         return self.controls_information.PVs.phase_set.get()
 
     @phase_set.setter
-    @check_state
     def phase_set(self, phase):
         if not isinstance([phase], float):
             return
@@ -139,11 +173,15 @@ class TCAV(Device):
         return self.controls_information.PVs.amp_fbenb.get()
 
     @amp_fbenb.setter
-    @check_state
-    def amp_fbenb(self, state: Union[str, int]):
-        if not isinstance(state, str) or not isinstance(state, int):
-            return
-        self.controls_information.PVs.amp_fbenb = state
+    def amp_fbenb(self, enum_str: str):
+        field_options = self.controls_information.amplitude_feedback_options
+        if not isinstance(enum_str, str):
+            raise TypeError(f"{enum_str} is not of type: str")
+        if enum_str not in field_options:
+            raise ValueError(
+                f"{enum_str} not in list of acceptable enumerate string PV values"
+            )
+        self.controls_information.PVs.amp_fbenb = enum_str
 
     @property
     def phase_fbenb(self):
@@ -151,52 +189,41 @@ class TCAV(Device):
         return self.controls_information.PVs.phase_fbenb.get()
 
     @phase_fbenb.setter
-    @check_state
-    def phase_fbenb(self, state: Union[str, int]):
-        if not isinstance(state, str) or not isinstance(state, int):
-            return
-        self.controls_information.PVs.phase_fbenb.put(state)
+    def phase_fbenb(self, enum_str: str):
+        field_options = self.controls_information.phase_feedback_options
+        if not isinstance(enum_str, str):
+            raise TypeError(f"{enum_str} is not of type: str")
+        if enum_str not in field_options:
+            raise ValueError(
+                f"{enum_str} not in list of acceptable enumerate string PV values"
+            )
+        self.controls_information.PVs.phase_fbenb.put(enum_str)
 
     @property
     def amp_fbst(self):
         """The state of the amplitude feedback"""
         return self.controls_information.PVs.amp_fbst.get()
 
-    @amp_fbst.setter
-    @check_state
-    def amp_fbst(self, state: Union[str, int]):
-        if not isinstance(state, str) or not isinstance(state, int):
-            return
-        self.controls_information.PVs.amp_fbst.put(state)
-
     @property
     def phase_fbst(self):
         """The state of the phase feedback"""
         return self.controls_information.PVs.phase_fbst.get()
-
-    @phase_fbst.setter
-    @check_state
-    def phase_fbst(self, state: Union[str, int]):
-        if not isinstance(state, str) or not isinstance(state, int):
-            return
-        self.controls_information.PVs.phase_fbst.put(state)
 
     @property
     def mode_config(self):
         """The current ATCA Trigger State"""
         return self.controls_information.PVs.mode_config.get(as_string=True)
 
-    @check_options("STDBY")
-    def standby(self):
-        self.controls_information.PVs.mode_config.put("STDBY")
-
-    @check_options("ACCEL")
-    def accelerate(self):
-        self.controls_information.PVs.mode_config.put("ACCEL")
-
-    @check_options("DIASBLE")
-    def disable(self):
-        self.controls_information.PVs.mode_config.put("DISABLE")
+    @mode_config.setter
+    def mode_config(self, enum_str):
+        field_options = self.controls_information.mode_config_options
+        if not isinstance(enum_str, str):
+            raise TypeError(f"{enum_str} is not of type: str")
+        if enum_str not in field_options:
+            raise ValueError(
+                f"{enum_str} not in list of acceptable enumerate string PV values"
+            )
+        self.controls_information.PVs.mode_config.put(enum_str)
 
     @property
     def l_eff(self):
@@ -213,16 +240,3 @@ class TCAV(Device):
     def rf_freq(self):
         """The Rf frequency of the TCAV in MHz"""
         return self.metadata.rf_freq
-
-
-# probably dont need a collection of tcavs, no area will have more than one
-class TCAVCollection(DeviceCollection):
-    devices: Dict[str, SerializeAsAny[TCAV]] = Field(alias="tcavs")
-
-    def __init__(self, *args, **kwargs):
-        super(TCAVCollection, self).__init__(*args, **kwargs)
-
-    @property
-    def tcavs(self) -> Dict[str, SerializeAsAny[TCAV]]:
-        """A dictionary (key=name, value=tcav) to directly access tcav objects"""
-        return self.devices
