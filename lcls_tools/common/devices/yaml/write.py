9481799cdef14051b325b3e580579daa301da205
Eloise Yang <98988862+eloiseyang@users.noreply.github.com>
2025-05-27 15:10:37 -0700

    lcls_tools.common.devices.yaml.writer now lets the user select a device to run.

--- lcls_tools/common/devices/yaml/write.py
+++ lcls_tools/common/devices/yaml/write.py
@@ -15,56 +15,65 @@ class YAMLWriter:
     def _is_area(self, area: str) -> bool:
         return area in self.generator.areas
 
-    def _constuct_yaml_contents(self, area: str) -> Dict[str, str]:
+    def _construct_yaml_contents(self, area: str, device_types: List[str] = None) -> Dict[str, str]:
+        if device_types is None:
+            device_types = ['magnets', 'screens', 'wires', 'lblms', 'bpms', 'tcavs']
+
         file_contents = {}
 
-        # Generate Magnet content
-        magnets = self.generator.extract_magnets(
-            area=area,
-        )
-        if magnets:
-            file_contents["magnets"] = magnets
+        if 'magnets' in device_types:
+            # Generate Magnet content
+            magnets = self.generator.extract_magnets(
+                area=area,
+            )
+            if magnets:
+                file_contents["magnets"] = magnets
 
-        # Generate Screens content
-        screens = self.generator.extract_screens(
-            area=area,
-        )
-        if screens:
-            file_contents["screens"] = screens
+        if 'screens' in device_types:
+            # Generate Screens content
+            screens = self.generator.extract_screens(
+                area=area,
+            )
+            if screens:
+                file_contents["screens"] = screens
 
-        # Generate Wire content
-        wires = self.generator.extract_wires(
-            area=area,
-        )
-        if wires:
-            file_contents["wires"] = wires
+        if 'wires' in device_types:
+            # Generate Wire content
+            wires = self.generator.extract_wires(
+                area=area,
+            )
+            if wires:
+                file_contents["wires"] = wires
 
-        # Generate LBLM content
-        lblms = self.generator.extract_lblms(
-            area=area,
-        )
-        if lblms:
-            file_contents["lblms"] = lblms
+        if 'lblms' in device_types:
+            # Generate LBLM content
+            lblms = self.generator.extract_lblms(
+                area=area,
+            )
+            if lblms:
+                file_contents["lblms"] = lblms
 
-        # Generate BPM content
-        bpms = self.generator.extract_bpms(
-            area=area,
-        )
-        if bpms:
-            file_contents["bpms"] = bpms
+        if 'bpms' in device_types:
+            # Generate BPM content
+            bpms = self.generator.extract_bpms(
+                area=area,
+            )
+            if bpms:
+                file_contents["bpms"] = bpms
 
-        # Generate BPM content
-        tcavs = self.generator.extract_tcavs(
-            area=area,
-        )
-        if tcavs and area == "DIAG0":
-            file_contents["tcavs"] = tcavs
+        if 'tcavs' in device_types:
+            # Generate BPM content
+            tcavs = self.generator.extract_tcavs(
+                area=area,
+            )
+            if tcavs and area == "DIAG0":
+                file_contents["tcavs"] = tcavs
 
         if file_contents:
             return file_contents
         return None
 
-    def write_yaml_file(self, area: Optional[str] = "GUNB") -> None:
+    def write_yaml_file(self, area: Optional[str] = "GUNB", device_types = None) -> None:
         if area not in self.generator.areas:
             raise RuntimeError(
                 f"Area {area} provided is not a known machine area.",
@@ -72,13 +81,17 @@ class YAMLWriter:
         filename = area + ".yaml"
         location = "lcls_tools/common/devices/yaml/"
         fullpath = os.path.join(location, filename)
-        yaml_output = self._constuct_yaml_contents(area=area)
+        yaml_output = self._constuct_yaml_contents(area=area, device_types=device_types)
         if yaml_output:
             with open(fullpath, "w") as file:
                 yaml.safe_dump(yaml_output, file)
 
-
-if __name__ == "__main__":
+def write(device_types: List[str] = None):
     writer = YAMLWriter()
     areas = writer.areas
     [writer.write_yaml_file(area) for area in areas]
+
+
+if __name__ == "__main__":
+    write()
+    
