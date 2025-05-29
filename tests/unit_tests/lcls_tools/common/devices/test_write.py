103df3565d9cee1e9cb9ccd002a919c4193876e6
Eloise Fae Yang <98988862+eloiseyang@users.noreply.github.com>
2025-05-29 11:38:34 -0700

    change dthe way unit_tests.lcls_tools.common.devices.test_write does mocking

--- tests/unit_tests/lcls_tools/common/devices/test_write.py
+++ tests/unit_tests/lcls_tools/common/devices/test_write.py
@@ -1,20 +1,21 @@
 import unittest
-from unittest.mock import Mock
+from unittest.mock import patch
 
-from lcls_tools.common.devices.yaml.write import YAMLWriter
+import lcls_tools.common.devices.yaml.write
 
 
+@patch("lcls_tools.common.devices.yaml.write.YAMLGenerator")
 class TestWrite(unittest.TestCase):
-    def test_single_device_types(self):
-        writer = YAMLWriter()
-        writer.generator = Mock()
+    def test_single_device_types(self, generator_class):
+        writer = lcls_tools.common.devices.yaml.write.YAMLWriter()
+        generator = writer.generator
         mock_dict = {
-            "magnets": writer.generator.extract_magnets,
-            "screens": writer.generator.extract_screens,
-            "wires": writer.generator.extract_wires,
-            "lblms": writer.generator.extract_lblms,
-            "bpms": writer.generator.extract_bpms,
-            "tcavs": writer.generator.extract_tcavs,
+            "magnets": generator.extract_magnets,
+            "screens": generator.extract_screens,
+            "wires": generator.extract_wires,
+            "lblms": generator.extract_lblms,
+            "bpms": generator.extract_bpms,
+            "tcavs": generator.extract_tcavs,
         }
         area = "None"
         for k, v in mock_dict.items():
