be01a06c8806648f6468b9095f0b88e5f5c5c4b7
Ryan Roussel <rroussel@slac.stanford.edu>
2025-10-03 10:53:17 -0500

    Update device.py

--- lcls_tools/common/devices/device.py
+++ lcls_tools/common/devices/device.py
@@ -14,6 +14,8 @@ class PVSet(lcls_tools.common.BaseModel):
 
     @field_validator("*", mode="before")
     def validate_pv_fields(cls, v: str) -> PV:
+        if v is None:
+            return None
         return PV(v)
 
     @field_serializer("*")
