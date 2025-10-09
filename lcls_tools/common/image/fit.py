38a4655578f7326371b6f279225dd8773b6c95ac
Chris Garnier <cgarnier@slac.stanford.edu>
2025-10-09 16:22:24 -0700

    commiting ImageProjectFit that uses model.gaussian for fitting, clean up next

--- lcls_tools/common/image/fit.py
+++ lcls_tools/common/image/fit.py
@@ -1,12 +1,12 @@
 from abc import ABC, abstractmethod
-from typing import Optional, List, Callable
+from typing import Optional, List
 
 import numpy as np
 from numpy import ndarray
 from pydantic import PositiveFloat, Field
 
 from lcls_tools.common.data.fit.method_base import MethodBase
-from lcls_tools.common.model.gaussian import fit
+from lcls_tools.common.data.fit.methods import GaussianModel
 from lcls_tools.common.data.fit.projection import ProjectionFit
 from lcls_tools.common.measurements.utils import NDArrayAnnotatedType
 import lcls_tools
@@ -20,7 +20,7 @@ class ImageFitResult(lcls_tools.common.BaseModel):
 
 
 class ImageProjectionFitResult(ImageFitResult):
-    projection_fit_method: Callable
+    projection_fit_method: MethodBase
     x_projection_fit_parameters: dict[str, float]
     y_projection_fit_parameters: dict[str, float]
 
@@ -54,23 +54,18 @@ class ImageProjectionFit(ImageFit):
     the x/y projections. The default configuration uses a Gaussian fitting of the
     profile with prior distributions placed on the model parameters.
     """
-    #Default is the fit method from model.gaussian, but any callable can be passed here.
-    #maybe this should be restricted to a special type of fit_callable.
-    #class fit(some_gauss):
-        #def __call__(proj_data):
-    # or we can decorate these methods in some way to make them a different type that we can restrict h
-    projection_fit_method: Optional[Callable] = fit
-    def p(self):
-        print('this is correct')
+
+    projection_fit_method: Optional[MethodBase] = GaussianModel(use_priors=True)
+
     def _fit_image(self, image: ndarray) -> ImageProjectionFitResult:
         x_projection = np.array(np.sum(image, axis=0))
         y_projection = np.array(np.sum(image, axis=1))
-        pos_x = np.linspace(0,len(x_projection), len(x_projection))
-        pos_y = np.linspace(0,len(y_projection), len(y_projection))                   
-        x_parameters = self.projection_fit_method(pos_x,x_projection)
-        y_parameters = self.projection_fit_method(pos_y,y_projection)
-        print(x_parameters)
-        print(y_parameters)
+
+        proj_fit = ProjectionFit(model=self.projection_fit_method)
+
+        x_parameters = proj_fit.fit_projection(x_projection)
+        y_parameters = proj_fit.fit_projection(y_projection)
+
         result = ImageProjectionFitResult(
             centroid=[x_parameters["mean"], y_parameters["mean"]],
             rms_size=[x_parameters["sigma"], y_parameters["sigma"]],
