b77afb5ab24310f7a322ee89545f3b60740d9d3d
Chris Garnier <cgarnier@slac.stanford.edu>
2025-11-25 10:08:44 -0800

    set signal to noise threshold to minimum value needed for invalidation test case to pass

--- tests/unit_tests/lcls_tools/common/image/test_image_fit.py
+++ tests/unit_tests/lcls_tools/common/image/test_image_fit.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 from lcls_tools.common.frontend.plotting.image import plot_image_projection_fit
 from lcls_tools.common.image.fit import ImageProjectionFit
+import pprint
 
 
 class TestImageProjectionFit(unittest.TestCase):
@@ -10,7 +11,7 @@ class TestImageProjectionFit(unittest.TestCase):
         test_image[40:60, 30:70] = 255
 
         result = ImageProjectionFit().fit_image(test_image)
-
+        pprint.pprint(result)
         assert np.allclose(result.centroid, [49.5, 49.5])
         assert np.allclose(result.rms_size, [15.09, 7.69], rtol=1e-2)
         assert np.allclose(result.total_intensity, test_image.sum())
@@ -30,10 +31,11 @@ class TestImageProjectionFit(unittest.TestCase):
         test_image[40:60, 30:70] = 255
 
         projection_fit = ImageProjectionFit(
+            signal_to_noise_threshold=5.0,
             validate_fit=True,
         )
         result = projection_fit.fit_image(test_image)
-
+        pprint.pprint(result)
         assert np.allclose(result.centroid, [np.nan, 49.5], equal_nan=True)
         assert np.allclose(
             result.rms_size, [np.nan, 7.692413702989997], rtol=1e-2, equal_nan=True
